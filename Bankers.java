facc7329393ce0893caba1340b06d108a07cdd13
arbaaz272 <43929415+arbaaz272@users.noreply.github.com>
2018-10-11 10:50:19 +0530

    Add files via upload

---
+++ Bankers.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class Bankers
+{
+public static void main(String args[])
+{
+int n,need[],allocate[],max[],avail[],np,nr;
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the no of processes and resources:");
+np=sc.nextInt();
+nr=sc.nextInt();
+need[]=int[np][nr];
+allocate[]=int[np][nr];
+max[]=int[np][nr];
+avail[]=int[0][nr];
+
+System.out.println("Enter the allocation matrix:");
+for(i=0,i<np,i++)
+for(j=0,j<nr,j++)
+allocate[n]=sc.nextInt();
+
+System.out.println("Enter the available matrix:");
+for(j=0,j<nr,j++)
+avail[n]=sc,nextInt();
+}
+public int calc_need();
+
+
